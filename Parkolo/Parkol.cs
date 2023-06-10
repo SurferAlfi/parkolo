f7aad09570d01d9265b0656dfd870df91576d1f5
Katona Ádám <106489877+SurferAlfi@users.noreply.github.com>
2023-06-10 07:17:03 +0200

    Add files via upload

---
+++ Parkolo/Parkol.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkolo
+{
+    enum PKategoria { Dolgozó, Vendég, Mozgássérült }
+    internal class Parkol
+    {
+        private int erkezes;
+        private int tavozas;
+        private string rsz;
+        private PKategoria pk;
+        public Parkol(string sor)
+        {
+            string[] d = sor.Split();
+            erkezes = int.Parse(d[1]);
+            tavozas = int.Parse(d[2]);
+            switch (d[3])
+            {
+                case "D":
+                    pk = PKategoria.Dolgozó;
+                    break;
+                case "V":
+                    pk = PKategoria.Vendég;
+                    break;
+                case "M":
+                    pk = PKategoria.Mozgássérült;
+                    break;
+                default:
+                    break;
+            }
+            rsz = d[4];
+        }
+        public string Rsz
+        {
+            get { return rsz; }
+            set
+            {
+                if (value.Length == 7 && value[3] == '-')
+                    rsz = value;
+            }
+        }
+        public int ParkolasiIdo
+        {
+            get
+            {
+                int parkido = tavozas - erkezes;
+                if (parkido > 0)
+                    return parkido;
+                return 0;
+            }
+        }
+        public int Fizetes()
+        {
+            int parkido = 0;
+            switch (pk)
+            {
+                case PKategoria.Dolgozó:
+                case PKategoria.Mozgássérült:
+                    parkido = 0;
+                    break;
+                case PKategoria.Vendég:
+                    if (ParkolasiIdo < 700)
+                        parkido = 0;
+                    else
+                        parkido = (ParkolasiIdo / 3600 + 1)*300;
+                        //vagy egészre kerekítés felfelé (így az egész órákkal is jól dolgozik)
+                        //parkido = (int)(Math.Ceiling((double)ParkolasiIdo / 3600) * 300);
+                    break;
+            }
+            return parkido;
+        }
+        public override string ToString()
+        {
+            return string.Format($"{rsz} {erkezes}-{tavozas} * {ParkolasiIdo} * {Fizetes()} Ft # {pk}");
+        }
+    }
+}
---
+++ Parkolo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Parkol> parkolok = new List<Parkol>();
+            try
+            {
+                StreamReader sr = new StreamReader("teszt.txt");
+                while (!sr.EndOfStream)
+                {
+                    parkolok.Add(new Parkol(sr.ReadLine()));
+                }
+                sr.Close();
+                Console.WriteLine("a járművek listája:");
+                List<string> list = new List<string>();
+                foreach (Parkol item in parkolok)
+                {
+                    list.Add(item.ToString());
+                }
+                foreach (var item in list)
+                    Console.WriteLine(item);
+
+                int lri = 0;
+                for (int i = 0; i < parkolok.Count; i++)
+                {
+                    if (parkolok[lri].Fizetes() > 0)
+                    {
+                        if (parkolok[i].Fizetes() > 0 && parkolok[lri].ParkolasiIdo > parkolok[i].ParkolasiIdo)
+                            lri = i;
+                    }
+                    else
+                    {
+                        lri = i;
+                    }
+                }
+                Console.WriteLine("\nA legrövidebb ideig parkoló fizető jármű: ");
+                Console.WriteLine(parkolok[lri]);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
